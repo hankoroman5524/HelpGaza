26f3ac20ffbf23fc7c7cdbc3a4f30443e02a0d65
hankoroman5524 <41116912+hankoroman5524@users.noreply.github.com>
2025-06-03 20:30:12 +0200

    get for people need

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ let app = express(ejs.resolveInclude);
 let ejs = require('ejs');
 const haikus = require('./haikus.json');
 const port = process.env.PORT || 3000;
-ConstantSourceNode
-app.use(express.static('public'))
+const express = ejs.public
+AAA express.static(Listen port 3000
+  internet index; 
+ )
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
